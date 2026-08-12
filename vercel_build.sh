d0d40801161d785ec02f96d3cab87e7e7cfaed4b
akhil <adithyaaroundai@gmail.com>
2026-08-12 14:25:46 +0530

    Add dummy var to .env to prevent 0-byte crash

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# This script creates an empty .env file so that `flutter build web` doesn't crash on Vercel.
-touch .env
+# This script creates a .env file so that `flutter build web` doesn't crash on Vercel.
+# We add a dummy variable so the file is not 0 bytes, ensuring Flutter bundles it.
+echo "BUILD_PLATFORM=VERCEL" > .env
 
 # Run the Flutter build command
 ./flutter/bin/flutter build web --release
